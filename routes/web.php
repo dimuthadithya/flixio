ad4346acb47dcf0ef49f6b30ab011acf9ceeb6c8
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-05-22 01:07:58 +0530

    Add admin dashboard view and update user routes for password management

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
      Route::put('/user/password', [UserController::class, 'updatePassword'])->name('password.update');
+ 
 });
 
 Route::get('/about', function () {
@@ -33,6 +34,10 @@ Route::get('/movies', [TmdbController::class, 'index'])->name('movies');
 Route::get('/user', function () {
     return view("user.dashboard");
 })->name('user.dashboard');
+
+Route::get('/user', function () {
+    return view("admin.dashboard");
+})->name('admin.dashboard');
 
 Route::get('/tvShows', [TvShowController::class, 'index'])->name('tvShows');
 Route::get('/watchList', [WatchListController::class, 'index'])->name('watchList');
