2acce43fa7190f3e55a5011ecbfee3cc3da70e86
dimuthx353 <dimuthx353@gmail.com>
2025-05-21 21:10:53 +0530

    Add role column to users table and implement UserSeeder for initial data population

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'John Doe',
+            'email' => 'user@gmail.com',
+            'password' => Hash::make('123')
+        ]);
+
+        User::create([
+            'name' => 'Jane Doe',
+            'email' => 'admin@gmail.com',
+            "role" => 'admin',
+            'password' => Hash::make('123')
+        ]);
+    }
+}
